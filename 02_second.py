04b184beb4a2dc388ef2a213b7b8da68064497af
Kaustubh <kaustubhr2001@gmail.com>
2024-02-18 00:10:18 +0530

    clousers and scopes

---
+++ 02_second.py
@@ -0,0 +1,26 @@
+# Scopes and Clousers
+
+x = 99
+
+def f1():
+    x = 88
+    def f2():
+        global x #x we use global keywork to access the global variable
+        print(x)
+    f2()
+
+
+f1() # 88
+
+
+
+#  Clousers
+
+def f3():
+    x = 88 #if this x is not availe then it will point to global x
+    def f4():
+        print(x)  #this will from an Clousers as it is accessing the variable from the outer function nd even if there is no x avalable in the outer function it will still work as it points to global x
+    return f4
+
+action = f3()
+action() # 88
