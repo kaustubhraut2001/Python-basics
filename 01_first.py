c8e259ddba1e9bbc7f2e4d8617c8e6a2bec42e49
Kaustubh <kaustubhr2001@gmail.com>
2024-02-17 23:34:09 +0530

    getting started

---
+++ 01_first.py
@@ -0,0 +1,8 @@
+# file is also iterable just like list, string, tuple etc.
+# __next__  and __iter__ are the two methods that are used to iterate over the file.
+# just smme we can write next() and iter() for file also.
+
+# FOR FILES:
+# readline for file handle the exception of EOF automatically.
+# but next() does not handle the exception of EOF automatically.
+
