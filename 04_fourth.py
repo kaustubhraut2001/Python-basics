90f3c0c1c84d0649d951b80366a78b0b5979a37f
Kaustubh <kaustubhr2001@gmail.com>
2024-02-19 00:13:54 +0530

    Update: decorators

--- 04_fourth.py
+++ 04_fourth.py
@@ -9,9 +9,26 @@ def timer(fn):
         return res
     return wrapper
 
-@timer
+@timer #this is a decorator this function will always call the timer function and pass the add function to it
 def add(a,b):
     time.sleep(2)
     return a+b
 
 add(2,4)
+
+
+
+
+def debug(fn):
+    def wrapper(*args , **kwargs):
+        args_value = ', '.join(str(arg) for arg in args)
+        kwargs_value = ', '.join(f"{key} = {value}" for key , value in kwargs.items())
+        print(args_value , kwargs_value)
+        return fn(*args , **kwargs)
+    return wrapper
+
+@debug
+def greet(name , msg = "Hey, Have a good day"):
+    print(f"Hello {name} {msg}")
+
+greet("Rahul")
