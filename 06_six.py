0c8c820e3254da48afbe4e5093f9b7eb1f3eaea9
Kaustubh <kaustubhr2001@gmail.com>
2024-02-23 00:13:25 +0530

    API data calling

--- 06_six.py
+++ 06_six.py
@@ -1,5 +1,24 @@
 # API handeling
 import requests
 
-r = requests.get("https://fakestoreapi.com/products")
-print(r.json() , r.status_code)
+def productsdetails():
+	r = requests.get("https://fakestoreapi.com/products")
+	# print(r.json() , r.status_code)
+
+	if r.status_code == 200:
+		for product in r.json():
+			print(product['title'])
+	else:
+		raise Exception("Fail to fetch")
+
+# productsdetails()
+
+def main():
+	try:
+		productsdetails()
+	except Exception as e:
+		print(e)
+
+
+if __name__ == "__main__":
+	main()
