f5b21deb4bd89a61245ca6c4cf9e3938d797a5ab
Kaustubh <kaustubhr2001@gmail.com>
2024-02-23 00:32:32 +0530

    Update

--- 07_seven.py
+++ 07_seven.py
@@ -17,5 +17,14 @@ cur.execute("""
 
 
 res = cur.execute("SELECT * FROM TEST")
-print(res.fetchall())
+#print(res.fetchall())
+data = [
+	('E', 5, 24),
+	('F', 6, 25),
+	('G', 7, 26),
+	('H', 8, 27)
+]
+cur.executemany("INSERT INTO TEST VALUES(?, ?, ?)", data)
+cur.execute("SELECT * FROM TEST")
+print(cur.fetchall())
 con.commit()
