74ab1eefa8ed339c5b808862d8d7d8a187af543e
Kaustubh <kaustubhr2001@gmail.com>
2024-03-03 22:15:40 +0530

    updations

--- 13_thirteen.py
+++ 13_thirteen.py
@@ -28,5 +28,5 @@ while True:
 	data = c.recv(1024).decode() #dara that we are recveing from the client
 	print(address , data)
 	c.send(bytes(f"Welcome to the serve Testin Sockets in python {address} and data is {data}", "utf-8")) # data that we are sen ing back to gthe server it will recive all these data
-
 	c.close()
+
