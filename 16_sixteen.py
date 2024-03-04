41522c591d1f6228c65b973881a505d0f7990603
Kaustubh <kaustubhr2001@gmail.com>
2024-03-04 23:20:41 +0530

    threading

--- 16_sixteen.py
+++ 16_sixteen.py
@@ -9,3 +9,33 @@ def topten():
 
 values = topten()
 print(values.__next__())
+
+
+
+
+# Erros and exception handeling
+
+
+#  Multi threading
+
+from  threading import *
+class Hello(Thread):
+	def run(self):
+		for i in range(5):
+			print("Hello")
+
+
+
+
+class Hi(Thread):
+	def run(self):
+		for i in range(5):
+			print("Hi")
+
+
+
+t1 = Hello()
+t2 = Hi()
+
+t1.start()
+t2.start()
