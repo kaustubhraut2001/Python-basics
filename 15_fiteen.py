5805ca45c6703f75d340f2cb8cc622a0b935c401
Kaustubh <kaustubhr2001@gmail.com>
2024-03-04 22:47:24 +0530

    Iterators

--- 15_fiteen.py
+++ 15_fiteen.py
@@ -27,6 +27,33 @@ nums = [8,7,6,5,4]
 
 it = iter(nums)
 
-print(it.__next__())
-print(it.__next__())
-print(it.__next__())
+#why we use it
+
+#print(it.__next__())
+#print(it.__next__())
+#print(it.__next__())
+
+#print(next(it))
+
+class topten:
+	def __init__(self ):
+		self.num = 1
+
+	def __iter__(self):
+		return self
+
+
+	def __next__(self):
+		if self.num <= 10:
+			val = self.num
+			self.num += 1
+			return val
+
+		else:
+			raise StopIteration
+
+
+values = topten()
+
+for i in values:
+	print(i)
