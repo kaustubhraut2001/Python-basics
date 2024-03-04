6af3c5a51acc9c4df010b97a86bef8488dbee486
Kaustubh <kaustubhr2001@gmail.com>
2024-03-04 23:33:01 +0530

    execution

---
+++ 17_seventeen.py
@@ -0,0 +1,4 @@
+# how python is getting executed  ->  Python's execution model combines elements of both compiled and interpreted languages. When a Python script runs, the source code is first compiled into an intermediate format called bytecode. This bytecode is then executed by the Python interpreter. This two-step process allows Python to balance the speed and efficiency of compiled languages with the flexibility and interactivity of interpreted languages, making it a versatile and widely-used programming language.
+
+# Compile -> Byte Code -> VM machine -> interpriteed code -> output
+
