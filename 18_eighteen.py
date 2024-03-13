b2055bfe3674c4c2b81190e4b88bd06d98102d66
Kaustubh <kaustubhr2001@gmail.com>
2024-03-13 11:53:13 +0530

    frozenset

---
+++ 18_eighteen.py
@@ -0,0 +1,11 @@
+a = list["asd" , 12 ,52]
+
+b = frozenset(a)
+
+print(b)
+
+print(type(b))
+
+'''A frozen set in Python is an immutable version of a set object. This means that once you've created a frozen set, you can't add, remove, or change its elements'''
+
+
